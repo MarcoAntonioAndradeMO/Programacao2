22c9f6f2c6bf43a5147da3d1500ad6a138916f42
Marco Andrade <marcoantonio.andrade05@gmail.com>
2024-08-04 12:19:28 -0300

    Questão 2 Lista 2

--- src/lista2/questao2.java
+++ src/lista2/questao2.java
@@ -1,7 +1,28 @@
 package lista2;
 
+import java.util.Scanner;
+
 public class questao2 {
+    public static String recebeNum(int[] nums) {
+        if (nums[0] > nums[1] && nums[0] > nums[2]) {
+            return "Condição Satisfeita";
+        } else {
+            return "Erro";
+        }
+    }
+
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        //Caso Teste
+        int num[] = new int[3];
+        System.out.println("Informe o Número: ");
+
+        for (int i = 0; i < num.length; i++) {
+            num[i] = scanner.nextInt();
+        }
+
+        String resultado = recebeNum(num);
+        System.out.println(resultado);
     }
 }
