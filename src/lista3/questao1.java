704d18073d1c1a51373b2e8671fb19920939ed9c
Marco Andrade <marcoantonio.andrade05@gmail.com>
2024-08-05 10:24:15 -0300

    Lista 3 Questão 1 e Teste

--- src/lista3/questao1.java
+++ src/lista3/questao1.java
@@ -1,4 +1,30 @@
 package lista3;
 
+import java.util.Scanner;
+
 public class questao1 {
+
+    public static int somaSeq(int[] nums) {
+        int somanum = 0;
+        for (int num : nums) {
+            somanum += num;
+        }
+        return somanum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] nums = new int[10];
+        System.out.println("Informe 10 números, em sequência: ");
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        int soma = somaSeq(nums);
+        System.out.println("Somatório: " + soma);
+
+    }
+
 }
